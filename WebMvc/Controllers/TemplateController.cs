1614c3004f67001557827705e5a77e57d330ead7
andreyu <andreyu@migmail.ru>
2013-09-05 08:59:02 +0400

    Реализовал меню для шаблонов

---
+++ WebMvc/Controllers/TemplateController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Reports.Presenters.UI.ViewModel;
+
+namespace WebMvc.Controllers
+{
+    public class TemplateController : Controller
+    {
+         [HttpGet]
+        public ActionResult Index()
+        {
+            return View(new TemplatesListModel());
+        }
+
+    }
+}
