b1ca299845095da9f8002015212ce3b0587b18a1
Andrey Ustinov <andreyu@migmail.ru>
2013-09-09 08:44:57 +0500

    Add attribute, change base class for controller

--- WebMvc/Controllers/TemplateController.cs
+++ WebMvc/Controllers/TemplateController.cs
@@ -1,19 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using Reports.Presenters.UI.ViewModel;
 
 namespace WebMvc.Controllers
 {
-    public class TemplateController : Controller
+    [Authorize]
+    public class TemplateController : BaseController
     {
-         [HttpGet]
+        [HttpGet]
         public ActionResult Index()
         {
             return View(new TemplatesListModel());
         }
-
     }
 }
