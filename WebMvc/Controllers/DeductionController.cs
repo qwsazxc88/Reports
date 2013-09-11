97f5192df794d907a0a0ff79424da5481623c28c
andreyu <andreyu@migmail.ru>
2013-09-11 09:35:02 +0400

    Добавил в меню удержания

---
+++ WebMvc/Controllers/DeductionController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using Reports.Core;
+using Reports.Presenters.UI.ViewModel;
+using WebMvc.Attributes;
+
+namespace WebMvc.Controllers
+{
+    public class DeductionController : BaseController
+    {
+        //
+        // GET: /Deduction/
+        [ReportAuthorize(UserRole.Accountant | UserRole.OutsourcingManager)]
+        public ActionResult Index()
+        {
+            return View( new DeductionsListModel());
+        }
+    }
+}
