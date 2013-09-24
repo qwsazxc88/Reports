d9a2f5bf9edbd84e689eba7190e10e76ab5bfccb
andreyu <andreyu@migmail.ru>
2013-09-24 08:32:48 +0400

    Перенес функции работы с выбиралкой департамента в HomeController

--- WebMvc/Controllers/HomeController.cs
+++ WebMvc/Controllers/HomeController.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 using Reports.Core;
+using Reports.Core.Dto;
 using Reports.Presenters.Services;
-using Reports.Presenters.Services.Impl;
+using Reports.Presenters.UI.Bl;
 using Reports.Presenters.UI.ViewModel;
 using WebMvc.Models;
 
@@ -14,18 +14,68 @@ namespace WebMvc.Controllers
     [Authorize]
     public class HomeController : BaseController
     {
+        protected IRequestBl requestBl;
+
+        public IRequestBl RequestBl
+        {
+            get
+            {
+                requestBl = Ioc.Resolve<IRequestBl>();
+                return Validate.Dependency(requestBl);
+            }
+        }
 
         public ActionResult Index(int? menuId)
         {
             //ViewBag.Message = "Welcome to ASP.NET MVC!";
-            IAuthenticationService service = Ioc.Resolve<IAuthenticationService>();
+            var service = Ioc.Resolve<IAuthenticationService>();
             IUser user = service.CurrentUser;
-            return View(new HomeModel { menuId = menuId.HasValue ? menuId.Value: 0 });
+            return View(new HomeModel {menuId = menuId.HasValue ? menuId.Value : 0});
         }
 
         public ActionResult About()
         {
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult DepartmentDialog(int id /*, int typeId*/)
+        {
+            try
+            {
+                //DepartmentTreeModel model = new DepartmentTreeModel { DepartmentID = id };
+                DepartmentTreeModel model = RequestBl.GetDepartmentTreeModel(id);
+                return PartialView(model);
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Exception", ex);
+                string error = "Ошибка при загрузке данных: " + ex.GetBaseException().Message;
+                return PartialView("DialogError", new DialogErrorModel {Error = error});
+            }
+        }
+
+        [HttpGet]
+        public ContentResult GetChildren(int parentId, int level)
+        {
+            DepartmentChildrenDto model;
+            try
+            {
+                model = RequestBl.GetChildren(parentId, level);
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Exception on GetChildren:", ex);
+                string error = ex.GetBaseException().Message;
+                model = new DepartmentChildrenDto
+                            {
+                                Error = string.Format("Ошибка: {0}", error),
+                                Children = new List<IdNameDto>()
+                            };
+            }
+            var jsonSerializer = new JavaScriptSerializer();
+            string jsonString = jsonSerializer.Serialize(model);
+            return Content(jsonString);
         }
     }
 }
