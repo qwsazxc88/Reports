7a505309eb0c4df43a0d13ff465aee815729fff5
andreyu <andreyu@migmail.ru>
2013-10-11 09:19:19 +0400

    Письмо от 10.10.2013 пункт 2

---
+++ WebMvc/Controllers/InfoController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using Reports.Presenters.UI.ViewModel;
+
+namespace WebMvc.Controllers
+{
+    [Authorize]
+    public class InfoController : BaseController
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(new InfoModel());
+        }
+    }
+}
