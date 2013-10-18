1e03dccb76d3cec424c52d8f51bf08d1f80ffd08
Andrey Ustinov <andreyu@migmail.ru>
2013-10-18 18:54:16 +0400

    Добавил классы для работы с базой.

---
+++ Reports.Core/Dao/ITerraPointDao.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Reports.Core.Domain;
+
+namespace Reports.Core.Dao
+{
+    public interface ITerraPointDao : IDao<TerraPoint>
+    {
+        IList<TerraPoint> GetDepartmentsTree(int tpId);
+    }
+}
---
+++ Reports.Core/Domain/TerraPoint.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace Reports.Core.Domain
+{
+    public class TerraPoint : AbstractEntityWithVersion//AbstractUsedEntity
+    {
+        #region Constants
+        #endregion
+
+        #region Fields
+        #endregion
+
+        #region Properties
+        public virtual string Code { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string ShortName { get; set; }
+        public virtual string Code1C { get; set; }
+        public virtual string ParentId { get; set; }
+        public virtual string Path { get; set; }
+        public virtual int? ItemLevel { get; set; }
+        public virtual DateTime? EndDate { get; set; }
+        #endregion
+
+        #region Constructors
+        #endregion
+        #region Methods
+        #endregion
+
+        #region System.Object overrides
+        #endregion
+
+        #region MetaData
+        #endregion
+    }
+}
