1129c311c40475a43785d42d7cd6f2a9a023b333
Andrey Ustinov <andreyu@migmail.ru>
2013-12-16 11:25:53 +0400

    Fix comment bug

--- WebMvc/Controllers/UserRequestController.cs
+++ WebMvc/Controllers/UserRequestController.cs
@@ -16,7 +16,7 @@ using WebMvc.Attributes;
 namespace WebMvc.Controllers
 {
     [PreventSpamAttribute]
-    [ReportAuthorize(UserRole.Employee | UserRole.Manager | UserRole.PersonnelManager | UserRole.Inspector | UserRole.Chief | UserRole.OutsourcingManager)]
+    [ReportAuthorize(UserRole.Employee | UserRole.Manager | UserRole.PersonnelManager | UserRole.Inspector | UserRole.Chief | UserRole.OutsourcingManager | UserRole.Director)]
     public class UserRequestController : BaseController
     {
         public const int MaxCommentLength = 256;
