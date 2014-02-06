0ab1a78915f220900b17fadaead144e3216a6ca5
marinru <marinru@front.ru>
2014-02-06 14:21:04 +0400

    Созданы заготовки сущностей обходного листа и комментария к нему.

---
+++ Reports.Core/Domain/ClearanceChecklist.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Reports.Core.Domain
+{
+    /// <summary>
+    /// Обходной лист при увольнении
+    /// </summary>
+    class ClearanceChecklist : AbstractEntityWithVersion
+    {
+        public virtual Dismissal Dismissal { get; set; }
+        public virtual IList<ClearanceChecklistComment> Comments { get; set; }
+    }
+}
---
+++ Reports.Core/Domain/ClearanceChecklistComment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Reports.Core.Domain
+{
+    /// <summary>
+    /// Комментарий к обходному листу
+    /// </summary>
+    class ClearanceChecklistComment : AbstractEntityWithVersion
+    {
+        public virtual User User { get; set; }
+        public virtual ClearanceChecklist ClearanceChecklist { get; set; }
+        public virtual string Comment { get; set; }
+        public virtual DateTime DateCreated { get; set; }
+    }
+}
