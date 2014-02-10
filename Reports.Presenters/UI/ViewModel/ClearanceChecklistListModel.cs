658120e545a86ef849e176a3822cd253a9b55a20
marinru <marinru@front.ru>
2014-02-10 17:13:00 +0400

    Added View Model for ClearanceChecklist list.

---
+++ Reports.Presenters/UI/ViewModel/ClearanceChecklistListModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Reports.Core.Dto;
+
+namespace Reports.Presenters.UI.ViewModel
+{
+    public class ClearanceChecklistListModel : BeginEndCreateDate
+    {
+        public int UserId { get; set; }
+
+        [Display(Name = "Структурное подразделение")]
+        public string DepartmentName { get; set; }
+        public int DepartmentId { get; set; }
+        public bool DepartmentReadOnly { get; set; }
+
+        [Display(Name = "Должность")]
+        public int PositionId { get; set; }
+        public IList<IdNameDto> Positions;
+
+        [Display(Name = "Период с")]
+        [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime? BeginDate { get; set; }
+        [Display(Name = "по")]
+        [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime? EndDate { get; set; }
+
+        [Display(Name = "Статус")]
+        public int StatusId { get; set; }
+        public IList<IdNameDto> Statuses;
+
+        public IList<VacationDto> Documents { get; set; }
+
+        [Display(Name = "ФИО сотрудника")]
+        public string UserName { get; set; }
+
+        public int SortBy { get; set; }
+        public bool? SortDescending { get; set; }
+    }
+}
