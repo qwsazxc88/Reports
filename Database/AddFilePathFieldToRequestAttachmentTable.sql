d82f9a7e3f3993adf70f4791fd63b06eec3d9759
Andrey Ustinov <andreyu@migmail.ru>
2014-05-13 15:19:15 +0400

    Add attachment files folder

---
+++ Database/AddFilePathFieldToRequestAttachmentTable.sql
@@ -0,0 +1,2 @@
+﻿alter table [dbo].[RequestAttachment] add [FilePath] nvarchar(1024) null
+alter table [dbo].[RequestAttachment] alter column [Context] varbinary(max) null
---
+++ Database/AddFilePathFieldToRequestPrintFormTable.sql
@@ -0,0 +1,2 @@
+﻿alter table [dbo].[RequestPrintForm] add [FilePath] nvarchar(1024) null
+alter table [dbo].[RequestPrintForm] alter column [Context] varbinary(max) null
