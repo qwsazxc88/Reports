84732d07b622fab7c7ce591bcbd2a5b186842e26
marinru <marinru@front.ru>
2014-09-17 10:43:28 +0400

    Изменения записей справочника контрагентов ЛС

--- Database/AddDataToPersonalAccountContractorTable.sql
+++ Database/AddDataToPersonalAccountContractorTable.sql
@@ -1,5 +1,5 @@
-insert into dbo.PersonalAccountContractor (Version, Code, Name) values
-	(1,N'00150',N'Нерезиденты МФ #3'),
-	(1,N'00151',N'Нерезиденты ЦФ #4'),
-	(1,N'000000001',N'Совкомбанк МФ #1'),
-	(1,N'000000011',N'Нерезиденты ЦФ #2')
+﻿insert into dbo.PersonalAccountContractor (Version, Code, Name) values
+	(1,N'00150',N'Нерезидент МФ #3'),
+	(1,N'00151',N'Нерезидент ЦФ #4'),
+	(1,N'000000001',N'МФ #1'),
+	(1,N'000000011',N'ЦФ #2')
