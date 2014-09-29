d627fe130cbf761a1d16946b0588cbac5bd7ea4e
marinru <marinru@front.ru>
2014-09-29 12:09:35 +0400

    Прикрепление сканов приказов на подпись для отпусков и увольнений

--- Reports.Presenters/UI/Bl/IRequestBl.cs
+++ Reports.Presenters/UI/Bl/IRequestBl.cs
@@ -20,7 +20,7 @@ namespace Reports.Presenters.UI.Bl
         void SetAllRequestListModel(AllRequestListModel model, bool hasError);
 
         VacationEditModel GetVacationEditModel(int id, int userId);
-        bool SaveVacationEditModel(VacationEditModel model, UploadFileDto fileDto, UploadFileDto orderScanFileDto, out string error);
+        bool SaveVacationEditModel(VacationEditModel model, UploadFileDto fileDto, UploadFileDto unsignedOrderScanFileDto, UploadFileDto orderScanFileDto, out string error);
         void ReloadDictionariesToModel(VacationEditModel model);
         bool ResetVacationApprovals(int id, out string error);
 
@@ -62,7 +62,7 @@ namespace Reports.Presenters.UI.Bl
         DismissalListModel GetDismissalListModel();
         void SetDismissalListModel(DismissalListModel model, bool hasError);
         DismissalEditModel GetDismissalEditModel(int id, int userId);
-        bool SaveDismissalEditModel(DismissalEditModel model, UploadFileDto fileDto, UploadFileDto orderScanFileDto, out string error);
+        bool SaveDismissalEditModel(DismissalEditModel model, UploadFileDto fileDto, UploadFileDto unsignedOrderScanFileDto, UploadFileDto orderScanFileDto, out string error);
         void ReloadDictionariesToModel(DismissalEditModel model);
                 
         ClearanceChecklistListModel GetClearanceChecklistListModel();
