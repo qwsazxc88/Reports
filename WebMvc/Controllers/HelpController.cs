db5139c4af66623ffc96d3a1790c267c3479e2ff
andreyu <andreyu@migmail.ru>
2014-10-08 09:47:06 +0400

    Добавил "Помощь" в меню

---
+++ WebMvc/Controllers/HelpController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace WebMvc.Controllers
+{
+    [Authorize]
+    public class HelpController : BaseController
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
