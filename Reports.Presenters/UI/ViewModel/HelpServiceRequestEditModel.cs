80fa9342824b865ceb503b51487c1b1e6d1826c5
andreyu <andreyu@migmail.ru>
2014-10-21 09:47:24 +0400

    Форма редактирования заявки на услуги

---
+++ Reports.Presenters/UI/ViewModel/HelpServiceRequestEditModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using Reports.Core.Dto;
+
+namespace Reports.Presenters.UI.ViewModel
+{
+    public class HelpServiceRequestEditModel:HelpUserInfoModel,IContainId
+    {
+        #region IContainId Members
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        #endregion
+        public int Version { get; set; }
+
+        [Display(Name = "Вид услуги")]
+        public int TypeId { get; set; }
+        public int TypeIdHidden { get; set; }
+        public IList<IdNameDto> Types;
+
+        [Display(Name = "Требования к справке")]
+        public string Requirements { get; set; }
+
+        [Display(Name = "Период")]
+        public int PeriodId { get; set; }
+        public int PeriodIdHidden { get; set; }
+        public IList<IdNameDto> Periods;
+
+        [Display(Name = "Срок изготовления")]
+        public int ProductionTimeTypeId { get; set; }
+        public int ProductionTimeTypeIdHidden { get; set; }
+        public IList<IdNameDto> ProductionTimeTypes;
+
+        [Display(Name = "Способ передачи")]
+        public int TransferMethodTypeId { get; set; }
+        public int TransferMethodTypeIdHidden { get; set; }
+        public IList<IdNameDto> TransferMethodTypes;
+    }
+}
---
+++ Reports.Presenters/UI/ViewModel/HelpUserInfoModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Reports.Presenters.UI.ViewModel
+{
+    public class HelpUserInfoModel : PreventDCModel
+    {
+        [Display(Name = "Дата")]
+        public string DateCreated { get; set; }
+
+        [Display(Name = "Номер заявки")]
+        public string DocumentNumber { get; set; }
+
+        [Display(Name = "ФИО сотрудника")]
+        public string UserName { get; set; }
+
+        [Display(Name = "Должность сотрудника")]
+        public string Position { get; set; }
+
+        [Display(Name = "Дирекция")]
+        public string Department1 { get; set; }
+
+        [Display(Name = "Подразделение")]
+        public string Department2 { get; set; }
+
+        [Display(Name = "Руководитель")]
+        public string ManagerName { get; set; }
+    }
+}
