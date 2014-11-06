c48eebc951116cee3dd4d3b9b1a8aa2b4756e81a
marinru <marinru@front.ru>
2014-11-06 17:06:38 +0300

    Переработаны права согласования кандидата вышестоящим руководителем

---
+++ Reports.Core/Dao/IMissionOrderRoleRecordDao.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using Reports.Core.Domain;
+using Reports.Core.Dto;
+
+namespace Reports.Core.Dao
+{
+    public interface IMissionOrderRoleRecordDao : IDao<MissionOrderRoleRecord>
+    {
+        IList<MissionOrderRoleRecord> GetRoleRecords(User user = null, string roleCode = null, User targetUser = null, Department targetDepartment = null);
+    }
+}
