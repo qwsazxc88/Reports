47a0ecda8b71a351d7be088ba0f53c8cdf01e00b
marinru <marinru@front.ru>
2014-11-30 15:33:29 +0300

    Ветка руководства исключена из определения автоматических прав

--- Reports.Core/Dao/Impl/DefaultDao.cs
+++ Reports.Core/Dao/Impl/DefaultDao.cs
@@ -532,6 +532,8 @@ namespace Reports.Core.Dao.Impl
                                 )
                             inner join dbo.Department managerManagerAccountDept
                                 on managerManagerAccount.DepartmentId = managerManagerAccountDept.Id
+                                  -- Исключить состоящих в ветке руководства
+                                  and managerManagerAccountDept.Path not like N'9900424.9900426.9900427.%'
                      
                             -- по ветке
                             inner join dbo.Department higherDept
@@ -548,11 +550,11 @@ namespace Reports.Core.Dao.Impl
                                         on currentUser.Id = {0}
                                     inner join dbo.Department employeeDept
                                     on employee.DepartmentId = employeeDept.Id 
+                                      -- Исключить состоящих в ветке руководства
+                                      and employeeDept.Path not like N'9900424.9900426.9900427.%'
                                     inner join dbo.Department higherDept
                                     on employeeDept.Path like higherDept.Path+N'%'
                                 where (employee.RoleId & 2) > 0
-                                    -- Сотрудник неявляется руководителем
-                                    and not exists (select 1 from dbo.Users employeeManagerAccount where employeeManagerAccount.Login like employee.Login+'R')
                                     and currentUser.DepartmentId = higherDept.Id
                                     and not currentUser.Login = employee.Login + N'R'";
 
