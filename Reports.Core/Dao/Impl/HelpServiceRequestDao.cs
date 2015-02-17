b3e6e03ac5f297e272d1570f6ab4145e6c916b9a
olegzagryazkin <olegzagryazkin@rambler.ru>
2015-02-17 09:06:05 +0300

    добавлена новая услуга, права для расчетчиков

--- Reports.Core/Dao/Impl/HelpServiceRequestDao.cs
+++ Reports.Core/Dao/Impl/HelpServiceRequestDao.cs
@@ -241,7 +241,7 @@ namespace Reports.Core.Dao.Impl
                     if (userId == 10)//расчетчики
                     {
                         sqlQuery = string.Format(sqlQuery, string.Empty);
-                        return @"  v.[TypeId] in (2, 4, 5, 7, 8, 10, 11, 16) ";
+                        return @"  v.[TypeId] in (2, 4, 5, 7, 8, 10, 11, 16, 21) ";
                     }
                     else
                     {
