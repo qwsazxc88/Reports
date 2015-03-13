a3f07448cbfe6a9ec662b6ec5cc59e7317e50e1b
artemb89 <artem_b89@mail.ru>
2015-03-13 17:01:46 +0300

    Письма отправляем только с рабочей версии.

--- Reports.Presenters/UI/Bl/IRequestBl.cs
+++ Reports.Presenters/UI/Bl/IRequestBl.cs
@@ -190,7 +190,7 @@ namespace Reports.Presenters.UI.Bl
         PrintMissionReportViewModel GetPrintMissionReportModel(int id);
         PrintMissionReportListViewModel GetPrintMissionReportListModel(int id,int reportId);
 
-        bool ExportFromMissionReportToDeduction(IEnumerable<int> DocIds);
+        bool ExportFromMissionReportToDeduction(IEnumerable<int> DocIds,bool EnableSendEmail);
 
         void SaveDocumentsToArchive(DeletePbRecordModel model);
         void SetPrintArchivistAddressModel(PrintArchivistAddressModel model);
