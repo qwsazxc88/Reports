43b3a34889265764f852323f0719f73559c33097
olegzagryazkin <olegzagryazkin@rambler.ru>
2015-04-08 15:44:40 +0300

    убрали поле ранг, на вкладке документов расширил права просмотра для учетки autsors

--- Database/Employment/SchemaEmployment.sql
+++ Database/Employment/SchemaEmployment.sql
@@ -1843,7 +1843,7 @@ GO
 
 
 
-	IF OBJECT_ID ('vwEmploymentFillState', 'V') IS NOT NULL
+IF OBJECT_ID ('vwEmploymentFillState', 'V') IS NOT NULL
 	DROP VIEW [dbo].[vwEmploymentFillState]
 GO
 
@@ -1860,7 +1860,7 @@ UNION ALL
 SELECT A.Id, B.IsFinal as GeneralFinal, C.IsFinal as PassportFinal, D.IsFinal as EducationFinal, E.IsFinal as FamilyFinal, F.IsFinal as MilitaryFinal, G.IsFinal as ExperienceFinal,
 			 H.IsFinal as ContactFinal, I.IsFinal as BackgroundFinal, 
 			 --cast(case when L.cnt = 8 then 1 else 0 end as bit) as CandidateApp,
-			 cast(case when L.cnt >= 1 then 1 else 0 end as bit) as CandidateApp,
+			 cast(case when L.cnt is null or (isnull(L.cnt, 0) <> 0)  then 0 else 1 end as bit) as CandidateApp,
 			 --кандидат полностью заполнил анкету
 			 cast(case when B.IsFinal = 1 and C.IsFinal = 1 and D.IsFinal = 1 and E.IsFinal = 1 and F.IsFinal = 1 and G.IsFinal = 1 and H.IsFinal = 1 and I.IsFinal = 1 then 1 else 0 end as bit) as CandidateReady,
 			 --согласование
@@ -1877,10 +1877,12 @@ INNER JOIN Contacts as H ON H.Id = A.ContactsId
 INNER JOIN BackgroundCheck as I ON I.Id = A.BackgroundCheckId
 INNER JOIN OnsiteTraining as J ON J.Id = A.OnsiteTrainingId
 INNER JOIN Managers as K ON K.Id = A.ManagersId
-LEFT JOIN (SELECT RequestId, count(RequestId) as cnt 
-					 FROM RequestAttachment 
-					 WHERE RequestType = 271--in (272, 273, 274, 275, 276, 277, 278, 279) 
-					 GROUP BY RequestId) as L ON L.RequestId = A.Id
+LEFT JOIN (SELECT A.CandidateId, A.cnt - isnull(B.cnt, 0) as cnt 
+					FROM (SELECT CandidateId, count(CandidateId) as cnt FROM EmploymentCandidateDocNeeded WHERE IsNeeded = 1 GROUP BY CandidateId) as A
+					LEFT JOIN (SELECT B.CandidateId, count(B.CandidateId) as cnt
+										FROM RequestAttachment as A
+										INNER JOIN EmploymentCandidateDocNeeded as B ON B.CandidateId = A.RequestId and B.DocTypeId = A.RequestType and B.IsNeeded = 1
+										GROUP BY B.CandidateId) as B ON B.CandidateId = A.CandidateId) as L ON L.CandidateId = A.Id	
 
 GO
 
