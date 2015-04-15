01bdab20080042105063b14f4d07600d4bd3ad57
olegzagryazkin <olegzagryazkin@rambler.ru>
2015-04-15 08:46:42 +0300

    перерыв

--- Reports.Presenters/UI/Bl/IStaffListBl.cs
+++ Reports.Presenters/UI/Bl/IStaffListBl.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Reports.Presenters.UI.ViewModel.StaffList;
+using Reports.Core.Domain;
 
 namespace Reports.Presenters.UI.Bl
 {
     public interface IStaffListBl : IBaseBl
     {
         TreeViewModel GetDepartmentList();
+        //IList<Department> GetDepartmentNodes(int DepartmentId);
     }
 }
--- Reports.Presenters/UI/Bl/Impl/StaffListBl.cs
+++ Reports.Presenters/UI/Bl/Impl/StaffListBl.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Reports.Presenters.UI.ViewModel.StaffList;
+using Reports.Core.Domain;
 
 namespace Reports.Presenters.UI.Bl.Impl
 {
@@ -11,8 +12,14 @@ namespace Reports.Presenters.UI.Bl.Impl
         public TreeViewModel GetDepartmentList()
         {
             TreeViewModel model = new TreeViewModel();
-            model.Departments = DepartmentDao.LoadAll();
+            model.Departments = DepartmentDao.GetDepartmentsTree((int)DepartmentDao.LoadAll().Where(x => x.ItemLevel == 1).Single().Id);
             return model;
+        }
+
+        public IList<Department> GetDepartmentNodes(int DepartmentId)
+        {
+            //IList<Department> depList = DepartmentDao.GetDepartmentsTree(DepartmentId);
+            return DepartmentDao.GetDepartmentsTree(DepartmentId);
         }
     }
 }
