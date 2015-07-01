b61c33c5152ce7084cc95ef14c5e08883372a873
olegzagryazkin <olegzagryazkin@rambler.ru>
2015-07-01 16:01:56 +0300

    1

---
+++ Reports.Presenters/UI/ViewModel/StaffList/TreeViewAjaxModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Reports.Core.Dto;
+using Reports.Core.Domain;
+
+namespace Reports.Presenters.UI.ViewModel.StaffList
+{
+    public class TreeViewAjaxModel
+    {
+        public IList<Department> Departments { get; set; }
+        public string DepId { get; set; }
+        //public string ParentId { get; set; }
+        public string FactAddress { get; set; }
+    }
+}
