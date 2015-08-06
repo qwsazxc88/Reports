d7e87f34a6ec080598dd05d3bb7af9b5b3ab8c17
olegzagryazkin <olegzagryazkin@rambler.ru>
2015-08-06 11:34:19 +0300

    скрипты

--- Database/StaffList/CreateSchemaStaffList.sql
+++ Database/StaffList/CreateSchemaStaffList.sql
@@ -2236,11 +2236,11 @@ GO
 --состояние заполнения анкет
 CREATE VIEW [dbo].[vwStaffListDepartment]
 AS
-SELECT C.Id, C.Code, C.Name, C.Code1C, C.ParentId, C.Path, C.ItemLevel, C.CodeSKD, C.Priority, B.NameShort as DepFingradName, B.NameComment as DepFingradNameComment, B.DepCode as FinDepPointCode
-FROM StaffDepartmentRequest as A
-INNER JOIN StaffDepartmentManagerDetails as B ON B.DepRequestId = A.Id
-INNER JOIN Department as C ON C.id = A.DepartmentId
-WHERE A.IsUsed = 1 
+SELECT A.Id, A.Code, A.Name, A.Code1C, A.ParentId, A.Path, A.ItemLevel, A.CodeSKD, A.Priority, C.NameShort as DepFingradName, C.NameComment as DepFingradNameComment, C.DepCode as FinDepPointCode
+FROM Department as A
+LEFT JOIN StaffDepartmentRequest as B ON B.DepartmentId = A.Id and B.IsUsed = 1 
+LEFT JOIN StaffDepartmentManagerDetails as C ON C.DepRequestId = B.Id
+--INNER JOIN Department as C ON C.id = A.DepartmentId
 GO
 
 
