da3ef775087aea8b1be439d08566393445ba4422
olegzagryazkin <olegzagryazkin@rambler.ru>
2015-10-23 14:34:32 +0300

    добавлены режимы работы кассы, банкомата и кэшина

--- Database/StaffList/Functions/fnGetDepartmentOperationModes.sql
+++ Database/StaffList/Functions/fnGetDepartmentOperationModes.sql
@@ -12,6 +12,7 @@ RETURNS
 (
 	 Id int 
 	,DMDetailId int
+	,ModeType int
 	,WeekDay int
 	,WorkBegin nvarchar(5)
 	,WorkEnd nvarchar(5)
@@ -23,28 +24,73 @@ AS
 BEGIN
 
 	INSERT INTO @ReturnTable
-	SELECT Id, DMDetailId, WeekDay, WorkBegin, WorkEnd, BreakBegin, BreakEnd, IsWorkDay 
+	SELECT Id, DMDetailId, ModeType, WeekDay, WorkBegin, WorkEnd, BreakBegin, BreakEnd, IsWorkDay 
 	FROM StaffDepartmentOperationModes
 	WHERE DMDetailId = @DMDetailId
 
 	IF NOT EXISTS (SELECT * FROM @ReturnTable)
 	BEGIN
 		INSERT INTO @ReturnTable
-		SELECT Id, DMDetailId, WeekDay, WorkBegin, WorkEnd, BreakBegin, BreakEnd, IsWorkDay FROM StaffDepartmentOperationModes WHERE DMDetailId = -1
+		SELECT Id, DMDetailId, ModeType, WeekDay, WorkBegin, WorkEnd, BreakBegin, BreakEnd, IsWorkDay FROM StaffDepartmentOperationModes WHERE DMDetailId = -1
 		UNION ALL
-		SELECT null, null, 1, null, null, null, null, 0
+		SELECT null, null, 1, 1, null, null, null, null, 0
 		UNION ALL
-		SELECT null, null, 2, null, null, null, null, 0
+		SELECT null, null, 1, 2, null, null, null, null, 0
 		UNION ALL
-		SELECT null, null, 3, null, null, null, null, 0
+		SELECT null, null, 1, 3, null, null, null, null, 0
 		UNION ALL
-		SELECT null, null, 4, null, null, null, null, 0
+		SELECT null, null, 1, 4, null, null, null, null, 0
 		UNION ALL
-		SELECT null, null, 5, null, null, null, null, 0
+		SELECT null, null, 1, 5, null, null, null, null, 0
 		UNION ALL
-		SELECT null, null, 6, null, null, null, null, 0
+		SELECT null, null, 1, 6, null, null, null, null, 0
+		--для кассы
 		UNION ALL
-		SELECT null, null, 7, null, null, null, null, 0
+		SELECT null, null, 2, 7, null, null, null, null, 0
+		UNION ALL
+		SELECT null, null, 2, 1, null, null, null, null, 0
+		UNION ALL
+		SELECT null, null, 2, 2, null, null, null, null, 0
+		UNION ALL
+		SELECT null, null, 2, 3, null, null, null, null, 0
+		UNION ALL
+		SELECT null, null, 2, 4, null, null, null, null, 0
+		UNION ALL
+		SELECT null, null, 2, 5, null, null, null, null, 0
+		UNION ALL
+		SELECT null, null, 2, 6, null, null, null, null, 0
+		UNION ALL
+		SELECT null, null, 2, 7, null, null, null, null, 0
+		--для банкомата
+		UNION ALL
+		SELECT null, null, 3, 1, null, null, null, null, 0
+		UNION ALL
+		SELECT null, null, 3, 2, null, null, null, null, 0
+		UNION ALL
+		SELECT null, null, 3, 3, null, null, null, null, 0
+		UNION ALL
+		SELECT null, null, 3, 4, null, null, null, null, 0
+		UNION ALL
+		SELECT null, null, 3, 5, null, null, null, null, 0
+		UNION ALL
+		SELECT null, null, 3, 6, null, null, null, null, 0
+		UNION ALL
+		SELECT null, null, 3, 7, null, null, null, null, 0
+		--для кэшина
+		UNION ALL
+		SELECT null, null, 4, 1, null, null, null, null, 0
+		UNION ALL
+		SELECT null, null, 4, 2, null, null, null, null, 0
+		UNION ALL
+		SELECT null, null, 4, 3, null, null, null, null, 0
+		UNION ALL
+		SELECT null, null, 4, 4, null, null, null, null, 0
+		UNION ALL
+		SELECT null, null, 4, 5, null, null, null, null, 0
+		UNION ALL
+		SELECT null, null, 4, 6, null, null, null, null, 0
+		UNION ALL
+		SELECT null, null, 4, 7, null, null, null, null, 0
 	END
 
 
