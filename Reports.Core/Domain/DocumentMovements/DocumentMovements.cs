251c4d71ec901756a9ebc46dc50a63de323d76de
artemb89 <artem_b89@mail.ru>
2015-11-02 14:18:28 +0300

    Начало....

---
+++ Reports.Core/Domain/DocumentMovements/DocumentMovements.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Reports.Core.Domain
+{
+    public class DocumentMovements: AbstractEntity
+    {
+        public virtual User Sender { get; set; }
+        public virtual User Receiver { get; set; }
+    }
+}
