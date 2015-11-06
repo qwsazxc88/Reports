96c913e746b67e4a61348944ac1a720619df04b2
olegzagryazkin <olegzagryazkin@rambler.ru>
2015-11-06 08:37:10 +0300

    1

---
+++ Reports.Core/Domain/Assistans.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Reports.Core.Domain
+{
+    /// <summary>
+    /// Помощники.
+    /// </summary>
+    public class Assistans : AbstractEntityWithVersion
+    {
+        /// <summary>
+        /// Согласовант.
+        /// </summary>
+        public virtual User ApproveUser { get; set; }
+        /// <summary>
+        /// Помощник.
+        /// </summary>
+        public virtual User AssistantUser { get; set; }
+        /// <summary>
+        /// Начало периода действия.
+        /// </summary>
+        public virtual DateTime? BeginDate { get; set; }
+        /// <summary>
+        /// Конец периода действия
+        /// </summary>
+        public virtual DateTime? EndDate { get; set; }
+        /// <summary>
+        /// Признак использования.
+        /// </summary>
+        public virtual bool IsUsed { get; set; }
+        public virtual User Creator { get; set; }
+        public virtual DateTime? CreateDate { get; set; }
+        public virtual User Editor { get; set; }
+        public virtual DateTime? EditDate { get; set; }
+    }
+}
---
+++ Reports.Core/Domain/PartialTypes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace Reports.Core.Domain
+{
+    /// <summary>
+    /// Справочник разделов приложения.
+    /// </summary>
+    public class PartialTypes : AbstractEntityWithVersion
+    {
+        public virtual string Name { get; set; }
+    }
+}
