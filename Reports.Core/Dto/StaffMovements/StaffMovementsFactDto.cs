20c0e9e33d15edb72f53684aff20ebeb7ccac595
artemb89 <artem_b89@mail.ru>
2015-12-11 15:33:03 +0300

    2423

---
+++ Reports.Core/Dto/StaffMovements/StaffMovementsFactDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Reports.Core.Dto
+{
+    public class StaffMovementsFactDto
+    {
+        public int Id { get; set; }
+        public DateTime SendTo1C { get; set; }
+        public UserDto User { get; set; }
+        public int StaffEstablishedPostRequestId { get; set; }
+        public int StaffMovementsId { get; set; }
+        public List<AdditionsDto> Additions { get; set; }
+
+    }
+}
