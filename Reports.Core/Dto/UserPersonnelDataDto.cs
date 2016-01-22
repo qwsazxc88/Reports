eb5e0190f01f9c2664c00fa9f9a5855767497875
artemb89 <artem_b89@mail.ru>
2016-01-22 17:10:13 +0300

    111

---
+++ Reports.Core/Dto/UserPersonnelDataDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Reports.Core.Dto
+{
+    public class UserPersonnelDataDto
+    {
+        public int UserId { get; set; }
+        public int PeopleId  { get; set; }
+        public string UserName { get; set; }
+        public string FIO { get; set; }
+        public string Position { get; set; }
+        public string Dep3Name { get; set; }
+        public string Dep7Name { get; set; }
+        public string Snils { get; set; }
+        public string INN { get; set; }
+        public DateTime? DateAccept { get; set; }
+        public DateTime? DateRelease { get; set; }
+        public bool GPDFlag { get; set; }
+        public bool UserFlag { get; set; }
+        public int DepId { get; set; }
+    }
+}
---
+++ Reports.Presenters/UI/ViewModel/UsersPersonnelDataViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Reports.Core.Dto;
+using System.ComponentModel.DataAnnotations;
+namespace Reports.Presenters.UI.ViewModel
+{
+    public class UsersPersonnelDataViewModel
+    {
+        [Display(Name="ФИО сотрудника")]
+        public string FIO { get; set; }
+        [Display(Name="Подразделение")]
+        public string DepartmentName { get; set; }
+        public int DepartmentId { get; set; }
+
+        public IList<UserPersonnelDataDto> Documents { get; set; }
+
+        public UsersPersonnelDataViewModel()
+        {
+            this.Documents=new List<UserPersonnelDataDto>();
+        }
+    }
+}
