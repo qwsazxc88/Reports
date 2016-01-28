5445df2da16c4b0e47b532f4c8c412b5f2b77c5d
artemb89 <artem_b89@mail.ru>
2016-01-28 16:16:20 +0300

    111

---
+++ Reports.Core/Dto/UserProfile/UserDocsDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Reports.Core.Dto.UserProfile
+{
+    public class UserDocsDto
+    {
+        string ShortLink { get; set; }
+        string Number { get; set; }
+        string Name { get; set; }
+    }
+}
